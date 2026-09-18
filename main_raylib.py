51fefe808ff2bf21e28d132b4500bd9e94a4e8fd
Chetan0402 <chetansharma67g@gmail.com>
2026-09-18 12:02:25 +0530

    take input in raylib

--- main_raylib.py
+++ main_raylib.py
@@ -1,4 +1,6 @@
-from pyray import *
+from pyray import *  # pyright: ignore[reportWildcardImportFromLibrary]
+
+search = ""
 
 init_window(800, 200, "Test")
 h, w = get_monitor_height(0), get_monitor_width(0)
@@ -6,8 +8,14 @@ set_window_size(w, h - 26)
 set_window_position(0, 26)
 set_window_state(ConfigFlags.FLAG_WINDOW_UNDECORATED)
 while not window_should_close():
+    key = get_char_pressed()
+    while key > 0:
+        search += chr(key)
+        key = get_char_pressed()
+
     begin_drawing()
-    draw_text("Hello", 0, 0, 24, WHITE)
+    clear_background(Color(30, 45, 50))
+    draw_text(search, 5, 5, 24, WHITE)
     end_drawing()
 
 close_window()

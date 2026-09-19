1f9a8c7ac091938591ce586e224e99394dce037e
Chetan0402 <chetansharma67g@gmail.com>
2026-09-19 12:08:12 +0530

    change font

--- main_raylib.py
+++ main_raylib.py
@@ -15,6 +15,7 @@ h, w = get_monitor_height(0), get_monitor_width(0)
 set_window_size(w, h - 26)
 set_window_position(0, 26)
 set_window_state(ConfigFlags.FLAG_WINDOW_UNDECORATED)
+FONT = load_font_ex("RobotoMonoNerdFont-Medium.ttf", 20, None, 0)
 while not window_should_close():
     key = get_char_pressed()
     while key > 0:
@@ -30,11 +31,13 @@ while not window_should_close():
 
     begin_drawing()
     clear_background(Color(30, 45, 50))
-    draw_text(search, 5, 5, 20, WHITE)
+    draw_text_ex(FONT, search, Vector2(5, 5), 20, 0, WHITE)
     i = 0
     for torrent in torrents:
         if torrent.name.lower().__contains__(search):
-            draw_text(torrent.name[:100], 5, 5 + 20 + i * 20, 20, WHITE)
+            draw_text_ex(
+                FONT, torrent.name[:100], Vector2(5, 5 + 20 + i * 20), 20, 0, WHITE
+            )
             i = i + 1
     end_drawing()
 
